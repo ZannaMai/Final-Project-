d71536d4e706e7b1cd623b9752c2397496ce18f9
ZannaMai <97138655+ZannaMai@users.noreply.github.com>
2022-03-24 16:26:59 -0600

    create __main__.py

---
+++ __main__.py
@@ -0,0 +1,28 @@
+from game.casting.cast import Cast
+from game.casting.snake import Actor
+from game.directing.director import Director
+from game.services.keyboard_service import KeyboardService
+from game.services.video_service import VideoService
+from game.shared.color import Color
+from game.shared.point import Point
+
+
+def main():
+    # create the cast
+    cast = Cast()
+
+    # start the game
+    keyboard_service = KeyboardService()
+    video_service = VideoService()
+
+    script = Script()
+    script.add_action("input", ControlActorsAction(keyboard_service))
+    script.add_action("update", MoveActorsAction())
+    script.add_action("update", HandleCollisionsAction())
+    script.add_action("output", DrawActorsAction(video_service))
+    
+    director = Director(video_service)
+    director.start_game(cast, script)
+
+if __name__ == "__main__":
+    main()
